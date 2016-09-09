4202c11c4adab04b855305d187e1921025e68bba
NIKI <nikiizvorski@gmail.com>
2016-09-09 11:23:04 +0100

    Adding Deck Class and Card Kotlin Classes

---
+++ src/Deck.kt
@@ -0,0 +1,15 @@
+/**
+ * Created by NIKI on 9/9/2016.
+ */
+class Deck {
+    val cards: Array<Card> = Array(52, fun (i : Int): Card {
+        val value = i % 13
+        val suit = when(i / 13){
+            0 -> "Clubs"
+            1 -> "Diamonds"
+            2 -> "Hearts"
+            else -> "Spades"
+        }
+        return Card(value, suit)
+    })
+}
