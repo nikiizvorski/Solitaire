4c8049c0cbb7f5e29eed4917b1746f677288f4ce
NIKI <nikiizvorski@gmail.com>
2016-09-09 11:45:35 +0100

    Refactor the Deck Class

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,9 +1,21 @@
+import java.util.*
+
 /**
  * Created by NIKI on 9/9/2016.
  */
 class Deck {
     //Using Lambda Expression Syntax and if you use it Kotlin will automatically generate the property
     val cards = Array(52, { Card(it % 13, getSuit(it)) })
+    var cardsInDeck: MutableList<Card> = cards.toMutableList()
+
+    fun drawCard(): Card = cardsInDeck.removeAt(0)
+
+    fun reset() {
+        cardsInDeck = cards.toMutableList()
+        Collections.shuffle(cardsInDeck)
+    }
+
+
 
     private fun  getSuit(i: Int) = when(i / 13) {
         0 -> "Clubs"
